3b60ff35ea668494a253f1eedbae3bfd564e7472
Zhdan <58386977+ZhdanDev@users.noreply.github.com>
2020-10-06 16:26:27 +0300

    Create Lab2.cs

---
+++ Lab2.cs
@@ -0,0 +1,27 @@
+//вариант 11
+
+using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        public static double GetSum(double n, double k)
+        {
+            double res = 0;
+            for (int i = 0; i < k; i++)
+            {
+                res += (Math.Pow(-1, Math.Pow(k,2) - 2*k) + 3) / (Math.Pow(2, k)-2);
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(GetSum(n,k));
+        }
+    }
+}
